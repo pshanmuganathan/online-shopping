a85fb403281151c109240372a5d77a20ccd0f41e
Prasanna Shanmuganathan <pshanmuganathan@hawk.iit.edu>
2018-03-21 13:51:14 -0500

    Added Styles and themes using bootstrap

--- shoppingwebsite/src/main/java/com/ps/shoppingwebsite/controller/PageContoller.java
+++ shoppingwebsite/src/main/java/com/ps/shoppingwebsite/controller/PageContoller.java
@@ -8,11 +8,30 @@ import org.springframework.web.servlet.ModelAndView;
 public class PageContoller {
 
 	
-	@RequestMapping(value={"/","/home","/index"})
+	@RequestMapping(value={"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to MVC");
+		mv.addObject("title","HomePage");
+		mv.addObject("homepage",true);
 		
+		return mv;
+		
+	}
+	
+	@RequestMapping(value={"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About");
+		mv.addObject("about",true);
+		return mv;
+		
+	}
+	
+	@RequestMapping(value={"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact");
+		mv.addObject("contact",true);
 		return mv;
 		
 	}
